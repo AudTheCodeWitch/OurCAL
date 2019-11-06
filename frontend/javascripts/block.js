79060ac67170a7c9d893d25062dd7487c77213ec
Audrea Cook <audreacwork@gmail.com>
2019-11-06 11:00:50 -0700

    Write setPixelColor function

--- frontend/javascripts/block.js
+++ frontend/javascripts/block.js
@@ -4,15 +4,31 @@ const createBlock = function () {
     for (noRows = 1; noRows < 26; noRows++) {
         const row = document.createElement('div');
         row.className = 'row';
-        row.id = `row${noRows}`;
+        row.id = `row-${noRows}`;
         let noCols;
         for (noCols = 1; noCols < 26; noCols++) {
             const pixel = document.createElement('div');
             pixel.className = 'pixel';
-            pixel.id = `pixel${noCols}`;
-            row.appendChild(pixel)
+            pixel.id = `pixel-${noCols}`;
+            pixel.addEventListener("click", function() {
+                setPixelColor(pixel)
+            });
+            row.appendChild(pixel);
         }
         block.appendChild(row)
     }
 };
+
+
+
+const setPixelColor = function(pixel) {
+    if (pixel.style.backgroundColor === 'white') {
+        pixel.style.backgroundColor = 'black'
+    } else {
+        pixel.style.backgroundColor = 'white'
+    }
+};
+
+
+
 createBlock();
