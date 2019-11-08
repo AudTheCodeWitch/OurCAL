5397255c7fa04de22fdf06eff50100fada8176b4
Audrea Cook <audreacwork@gmail.com>
2019-11-08 13:02:44 -0700

    Stub Block actions

--- backend/our-cal-api/app/controllers/blocks_controller.rb
+++ backend/our-cal-api/app/controllers/blocks_controller.rb
@@ -1,2 +1,27 @@
 class BlocksController < ApplicationController
+
+  def index
+    blocks = Block.all
+    render json: blocks, include: [:pixels]
+  end
+
+  def show
+    block = Block.find_by(id: params[:id])
+    if block
+      render json: block, include: [:user, :difficulty, :pixels]
+    else
+      render json: { message: 'No block found with that id' }
+    end
+  end
+
+  def create
+    block = Block.new
+    user = User.find_or_create_by(user_params)
+
+  end
+
+  def destroy
+
+  end
+
 end
