f2a4030894e6827af47fbc3ea98ae3d73a504330
Audrea Cook <audreacwork@gmail.com>
2019-11-08 14:39:26 -0700

    Add color_variable to Pixels

--- backend/our-cal-api/app/serializers/pixel_serializer.rb
+++ backend/our-cal-api/app/serializers/pixel_serializer.rb
@@ -1,5 +1,5 @@
 class PixelSerializer
   include FastJsonapi::ObjectSerializer
-  attributes :x, :y, :color
+  attributes :x, :y, :color, :color_variable
   belongs_to :block
 end
--- backend/our-cal-api/db/schema.rb
+++ backend/our-cal-api/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_05_193035) do
+ActiveRecord::Schema.define(version: 2019_11_08_205649) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -38,6 +38,7 @@ ActiveRecord::Schema.define(version: 2019_11_05_193035) do
     t.bigint "block_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "color_variable"
     t.index ["block_id"], name: "index_pixels_on_block_id"
   end
 
