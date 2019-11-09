0d91b855a6ae133e51cd5bf4017d61d7fd6265c0
Audrea Cook <audreacwork@gmail.com>
2019-11-09 14:51:25 -0700

    Add ability to destroy pixels with block

--- backend/our-cal-api/app/models/block.rb
+++ backend/our-cal-api/app/models/block.rb
@@ -1,7 +1,7 @@
 class Block < ApplicationRecord
   belongs_to :user
   belongs_to :difficulty
-  has_many :pixels
+  has_many :pixels, :dependent => :destroy
   accepts_nested_attributes_for :pixels
 
 end
