ed8b425f4269e786caca3fa6142769ec69114f26
Audrea Cook <audreacwork@gmail.com>
2019-12-12 14:58:12 -0700

    Transfer BlockHeader text to React

--- frontend/our-cal/src/containers/NewBlockContainer.js
+++ frontend/our-cal/src/containers/NewBlockContainer.js
@@ -1,10 +1,12 @@
 import React, {Component} from 'react';
+import BlockHeader from "../components/BlockHeader";
 
 class NewBlockContainer extends Component {
 
     render() {
         return (
             <div className='block-container'>
+                <BlockHeader />
                 New block goes here
             </div>
         );
