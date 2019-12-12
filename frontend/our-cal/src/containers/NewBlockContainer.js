eaa89392ff852e7638354f549756f0a08d33febf
Audrea Cook <audreacwork@gmail.com>
2019-12-12 14:07:00 -0700

    Stub NewBlockContainer

---
+++ frontend/our-cal/src/containers/NewBlockContainer.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+
+class NewBlockContainer extends Component {
+
+    render() {
+        return (
+            <div>
+                Hello World
+
+            </div>
+        );
+    }
+}
+
+export default NewBlockContainer;
