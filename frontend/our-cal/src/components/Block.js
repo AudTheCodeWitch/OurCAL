476cd7082a6bc3ecc29154750cf8a4c8bce95c54
Audrea Cook <audreacwork@gmail.com>
2019-12-13 12:37:56 -0700

    Add createBlock() and createColumns()

--- frontend/our-cal/src/components/Block.js
+++ frontend/our-cal/src/components/Block.js
@@ -3,11 +3,33 @@ import Pixel from "./Pixel";
 
 class Block extends Component {
 
+    createColumns = () => {
+        const columns = new Array(25);
+        const items = [];
+        for (const [index] of columns.entries()) {
+            let p = <Pixel/>;
+            // TODO Add id and class names to pixels
+            items.push(p)
+        }
+        return items
+    };
+
+    createBlock = () => {
+        const rows = new Array(25);
+        const items = [];
+        for (const [index] of rows.entries()) {
+            items.push(<div key={index} className='row' id={'row-'+(index+1)}>
+                {this.createColumns()}
+            </div>)
+        }
+        return items
+    }
+
 
     render() {
         return (
-            <div className='template'>
-                <pixel/>
+            <div className='template block'>
+                {this.createBlock()}
             </div>
         );
     }
