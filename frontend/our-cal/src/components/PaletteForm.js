8488cf8abd9f170fbabcf98da3136d59b99374cf
Audrea Cook <audreacwork@gmail.com>
2019-12-13 14:07:46 -0700

    Render Palette pens

--- frontend/our-cal/src/components/PaletteForm.js
+++ frontend/our-cal/src/components/PaletteForm.js
@@ -1,14 +1,42 @@
 import React, {Component} from 'react';
+import Palette from "./Palette";
 
 class PaletteForm extends Component {
+
+    createPenSelector = () => {
+        const array = new Array(5);
+        const pens = [];
+        for (const [index] of array.entries()) {
+            pens.push(
+                <li key={index}>
+                    {/*TODO Pass index+2 as props to palette*/}
+                    {/*<button>Color {index+2}</button>*/}
+                    <input type='radio' name='pen' id={'pen '+(index+1)}/>
+                    <br/>
+                    <br/>
+                    <Palette />
+                </li>
+            )
+
+        }
+        return pens
+    };
+
+
 
     render() {
         return (
             <div>
                 <form className="palette-form">
                     <ul id='palette'>
+                        <li>
+                            Select your pen:
+                            <br/>
+                            <br/>
+                            <Palette />
+                        </li>
                     {/*TODO : Render Color Pickers*/}
-                    Color Pickers Go Here
+                        {this.createPenSelector()}
                     </ul>
                 </form>
                 <div id='pen'/>
