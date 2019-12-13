973257e3585e1561db0ed606892908bacd52b4e2
Audrea Cook <audreacwork@gmail.com>
2019-12-13 11:24:23 -0700

    Stub Block

---
+++ frontend/our-cal/src/components/Block.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+import Pixel from "./Pixel";
+
+class Block extends Component {
+
+
+    render() {
+        return (
+            <div className='template'>
+                <pixel/>
+            </div>
+        );
+    }
+}
+
+export default Block;
