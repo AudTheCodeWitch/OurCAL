5028fcb1503b08a6a17fdbf88c02e20ed4f91a93
Audrea Cook <audreacwork@gmail.com>
2019-12-13 10:05:51 -0700

    Change App function to class component

--- frontend/our-cal/src/App.js
+++ frontend/our-cal/src/App.js
@@ -4,23 +4,25 @@ import NewBlockContainer from "./containers/NewBlockContainer";
 import CardContainer from "./containers/CardContainer";
 import CodeWitchLogo from './CodeWitchLogo.png'
 
-function App() {
-  return (
-    <div className="App">
-        <header>
-            <h1>OurCal: Community Graphghan</h1>
-        </header>
-        <NewBlockContainer />
-        <br/>
-        <CardContainer />
-        <footer>
-            <ul>
-                <li>©2019 Audrea Cook</li>
-                <li><img src={CodeWitchLogo} alt="AudTheCodeWitch Logo" height="50px"/></li>
-            </ul>
-        </footer>
-    </div>
-  );
+class App extends React.Component {
+    render() {
+        return (
+            <div className="App">
+                <header>
+                    <h1>OurCal: Community Graphghan</h1>
+                </header>
+                <NewBlockContainer />
+                <br/>
+                <CardContainer />
+                <footer>
+                    <ul>
+                        <li>©2019 Audrea Cook</li>
+                        <li><img src={CodeWitchLogo} alt="AudTheCodeWitch Logo" height="50px"/></li>
+                    </ul>
+                </footer>
+            </div>
+        );
+    }
 }
 
 export default App;
