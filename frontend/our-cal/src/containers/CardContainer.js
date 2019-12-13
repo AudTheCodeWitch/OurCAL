23f2ad40834d7aea331dbc22269def39af16538c
Audrea Cook <audreacwork@gmail.com>
2019-12-13 14:58:00 -0700

    Render cards through createCards()

--- frontend/our-cal/src/containers/CardContainer.js
+++ frontend/our-cal/src/containers/CardContainer.js
@@ -2,17 +2,24 @@ import React, {Component} from 'react';
 import Card from "../components/Card";
 
 class CardContainer extends Component {
+
+    createCards = () => {
+        const cards = new Array(8);
+        const items = [];
+        for (const [index] of cards.entries()) {
+            let c = <Card/>;
+            items.push(c)
+        }
+        return items
+    };
+
     render() {
         return (
             <div className='card-container'>
                 <h2>View Blocks</h2>
                 <div className='cards'>
                     {/*TODO write map card function*/}
-                    <Card />
-                    <Card />
-                    <Card />
-                    <Card />
-                    <Card />
+                    {this.createCards()}
                 </div>
             </div>
         );
