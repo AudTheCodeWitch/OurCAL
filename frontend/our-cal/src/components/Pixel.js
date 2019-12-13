aea3d54234aef8df049ed804a167cec68ad9389f
Audrea Cook <audreacwork@gmail.com>
2019-12-13 12:36:41 -0700

    Remove dummy code

--- frontend/our-cal/src/components/Pixel.js
+++ frontend/our-cal/src/components/Pixel.js
@@ -2,25 +2,10 @@ import React, {Component} from 'react';
 
 class Pixel extends Component {
 
-    constructor(props) {
-        super(props);
-        this.state = {
-            color: props.value
-        }
-    }
-
-    handleClick = () => {
-        this.setState({
-            color: '#333'
-        })
-    }
-
     render() {
         return (
-            <div
-                className="pixel"
-                style={{backgroundColor: this.state.color}}
-                onClick={this.handleClick}>
+            <div className="pixel">
+
             </div>
         );
     }
