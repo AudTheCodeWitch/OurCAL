05c3f7ca8de1139b05aedcf9abf256a57be38cec
Audrea Cook <audreacwork@gmail.com>
2019-12-13 10:06:27 -0700

    Stub Card component

---
+++ frontend/our-cal/src/components/Card.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class Card extends Component {
+    render() {
+        return (
+            <div className='card'>
+                Card 1
+            </div>
+        );
+    }
+}
+
+export default Card;
