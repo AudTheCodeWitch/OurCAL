54cfca31ee31b8c5138b0a394156060130a510cf
Audrea Cook <audreacwork@gmail.com>
2019-12-13 14:56:33 -0700

    Fix icon and remove extra class

--- frontend/our-cal/src/components/Card.js
+++ frontend/our-cal/src/components/Card.js
@@ -1,14 +1,14 @@
 import React, {Component} from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faTimesCircle } from '@fortawesome/free-solid-svg-icons'
+import { faTimesCircle } from '@fortawesome/free-regular-svg-icons'
 import CompleteBlock from "./CompleteBlock";
 
 class Card extends Component {
     render() {
         return (
             <div className='card'>
-                <button className='far fa-times-circle'>
-                    <FontAwesomeIcon icon={faTimesCircle} />
+                <button className='fa-times-circle'>
+                    <FontAwesomeIcon icon={ faTimesCircle } />
                 </button>
                 <h3>Block Name</h3>
                 <div className='block'>
