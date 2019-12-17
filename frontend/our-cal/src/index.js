40b40b49845affc9afe3ac08b07605c1f815dd2a
Audrea Cook <audreacwork@gmail.com>
2019-12-17 10:06:04 -0700

    Create store and wrap with Provider

--- frontend/our-cal/src/index.js
+++ frontend/our-cal/src/index.js
@@ -8,7 +8,17 @@ import { thunk } from 'redux-thunk'
 import { Provider } from 'react-redux'
 import rootReducer from './reducers/reducers'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore(
+    rootReducer,
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
+
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>,
+    document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
