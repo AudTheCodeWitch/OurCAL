679dc24cea5778c7d1208e0a05f23932d15afef4
Audrea Cook <audreacwork@gmail.com>
2019-12-17 15:15:32 -0700

    Add compose and apply thunk

--- frontend/our-cal/src/index.js
+++ frontend/our-cal/src/index.js
@@ -3,14 +3,17 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import { createStore, applyMiddleware } from "redux";
-import { thunk } from 'redux-thunk'
+import { createStore, applyMiddleware, compose } from "redux";
+import thunk from 'redux-thunk'
 import { Provider } from 'react-redux'
 import rootReducer from './reducers/reducers'
 
+const composeEnhancers= window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
 const store = createStore(
     rootReducer,
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    composeEnhancers(applyMiddleware(thunk))
+
 );
 
 ReactDOM.render(
