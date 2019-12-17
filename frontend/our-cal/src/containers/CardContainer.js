59b6eb9c15f7dc4e0946e4cd734ac524036ef2e1
Audrea Cook <audreacwork@gmail.com>
2019-12-17 10:09:14 -0700

    Connect to store

--- frontend/our-cal/src/containers/CardContainer.js
+++ frontend/our-cal/src/containers/CardContainer.js
@@ -1,12 +1,13 @@
 import React, {Component} from 'react';
 import Card from "../components/Card";
+import { connect } from 'react-redux'
 
 class CardContainer extends Component {
 
-    createCards = () => {
-        const cards = new Array(8);
+    createCards = (blocks) => {
+        // const cards = new Array(8);
         const items = [];
-        for (const [index] of cards.entries()) {
+        for (const [index] of blocks.entries()) {
             let c = <Card/>;
             items.push(c)
         }
@@ -14,16 +15,21 @@ class CardContainer extends Component {
     };
 
     render() {
+        console.log(this.props.blocks)
         return (
             <div className='card-container'>
                 <h2>View Blocks</h2>
                 <div className='cards'>
                     {/*TODO write map card function*/}
-                    {this.createCards()}
+                    {this.createCards(this.props.blocks)}
                 </div>
             </div>
         );
     }
 }
 
-export default CardContainer;
+const mapStateToProps = (state) => {
+    return { blocks: state.blocks.all }
+};
+
+export default connect(mapStateToProps)(CardContainer);
