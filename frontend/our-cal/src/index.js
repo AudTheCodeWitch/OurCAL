b9b43a76a6a8d6f1170359a2fe9ab844afbbdbbd
Audrea Cook <audreacwork@gmail.com>
2019-12-17 10:05:34 -0700

    Import packages

--- frontend/our-cal/src/index.js
+++ frontend/our-cal/src/index.js
@@ -3,6 +3,10 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { createStore, applyMiddleware } from "redux";
+import { thunk } from 'redux-thunk'
+import { Provider } from 'react-redux'
+import rootReducer from './reducers/reducers'
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
