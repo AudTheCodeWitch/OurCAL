1caec9a7863116b5712e7b0e35d9e147f43bfe42
Audrea Cook <audreacwork@gmail.com>
2019-12-19 16:13:13 -0700

    Rename rootReducer file

--- frontend/our-cal/src/index.js
+++ frontend/our-cal/src/index.js
@@ -6,7 +6,7 @@ import * as serviceWorker from './serviceWorker';
 import { createStore, applyMiddleware, compose } from "redux";
 import thunk from 'redux-thunk'
 import { Provider } from 'react-redux'
-import rootReducer from './reducers/reducers'
+import rootReducer from './reducers/rootReducer'
 
 const composeEnhancers= window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
