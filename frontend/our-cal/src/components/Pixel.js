2d8b7773f81cbaad356f84d8794e4bd1cfcdbbec
Audrea Cook <audreacwork@gmail.com>
2019-12-19 16:33:32 -0700

    Add else to if statement
    Remove comments

--- frontend/our-cal/src/components/Pixel.js
+++ frontend/our-cal/src/components/Pixel.js
@@ -5,42 +5,30 @@ import { colorPixel } from "../actions/colorPixel";
 
 class Pixel extends Component {
 
-    // constructor(props) {
-    //     super(props);
-    //     this.state = {
-    //         color: props.colors.bg
-    //     };
-    // }
-
     handleClick = (pen, event) => {
         if (pen === '') {
-            alert('Please select a pen, first!')
-        }
-
-        let pixel = event.target;
-        let color;
-
-        if (pixel.className === `pixel ${pen}`) {
-            pixel.className=`pixel bg`;
-            color = this.props.colors.bg
-
+            alert('Please select a pen first!')
         } else {
-            pixel.className=`pixel ${pen}`;
-            color = this.props.colors[pen]
-        }
-        pixel.style.backgroundColor = color;
-        // this.setState({
-        //     color: color
-        // })
-        this.props.colorPixel(pixel.id.split('-')[1], pen, color)
+            let pixel = event.target;
+            let color;
 
+            if (pixel.className === `pixel ${pen}`) {
+                pixel.className = `pixel bg`;
+                color = this.props.colors.bg
+
+            } else {
+                pixel.className = `pixel ${pen}`;
+                color = this.props.colors[pen]
+            }
+            pixel.style.backgroundColor = color;
+            this.props.colorPixel(pixel.id.split('-')[1], pen, color)
+        }
     };
 
     componentDidMount() {
         let x = this.props.column + 1;
         let y = 25 - this.props.row;
         let pixel = {
-            // coord: x + ', ' + y,
             x: x,
             y: y,
             color: this.props.colors.bg,
@@ -53,7 +41,6 @@ class Pixel extends Component {
     }
 
     render() {
-        // console.log('rendering')
         const { location, column, row, pen, colors, color } = this.props;
         if (location === 'Template') {
             return (
