a6cae5a607c357a851db52333ed38ca1cfea88fd
Audrea Cook <audreacwork@gmail.com>
2019-12-18 17:05:09 -0700

    Add handleChange and handleSubmit to form

--- frontend/our-cal/src/components/BlockForm.js
+++ frontend/our-cal/src/components/BlockForm.js
@@ -1,28 +1,54 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
+import { connect } from "react-redux";
+import { postBlock } from "../actions/postBlock";
 
 class BlockForm extends Component {
+
+    constructor() {
+        super();
+        this.state = {
+            blockName: '',
+            difficulty: '',
+            username: '',
+            email: ''
+        };
+    }
+
+    handleChange = (event) => {
+        this.setState({
+            [event.target.name]: event.target.value
+        });
+    };
+
+    handleSubmit = (event) => {
+        event.preventDefault();
+        this.props.postBlock(this.state, this.props.block)
+    };
     render() {
         return (
             <div className='block-details'>
-                <form className="block-form">
+                <form onSubmit={(event) => this.handleSubmit(event)} className="block-form">
                     <h2>Block Details</h2>
                     Block name:<br/>
-                    <input type="text" name="blockName" /><br/>
-                    <input type="radio" name="difficulty" value="Beginner" />
+                    <input onChange={(event) => this.handleChange(event)} type="text" name="blockName" /><br/>
+                    <input onChange={(event) => this.handleChange(event)} type="radio" name="difficulty" value="Beginner" />
                         Beginner<br/>
-                    <input type="radio" name="difficulty" value="Intermediate" />
+                    <input onChange={(event) => this.handleChange(event)} type="radio" name="difficulty" value="Intermediate" />
                         Intermediate<br/>
-                    <input type="radio" name="difficulty" value="Advanced" />
+                    <input onChange={(event) => this.handleChange(event)} type="radio" name="difficulty" value="Advanced" />
                         Advanced<br/><br/>
                     Username:<br/>
-                    <input type="text" name="username" /><br/>
+                    <input onChange={(event) => this.handleChange(event)} type="text" name="username" /><br/>
                     Email:<br/>
-                    <input type="email" name="email" /><br/><br/>
+                    <input onChange={(event) => this.handleChange(event)} type="email" name="email" /><br/><br/>
                     <input id='create' type="submit" value="Create Block" />
                 </form>
             </div>
         );
     }
 }
+const mapStateToProps = (state) => {
+    return { block: state.blockTemplate.blockTemplate }
+};
 
-export default BlockForm;
+export default connect(mapStateToProps,{postBlock})(BlockForm);
