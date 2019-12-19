33d6c829f03361a79f56ec774980d2bc01f900a6
Audrea Cook <audreacwork@gmail.com>
2019-12-19 16:04:19 -0700

    Clear form and block template on submit

--- frontend/our-cal/src/components/BlockForm.js
+++ frontend/our-cal/src/components/BlockForm.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from "react-redux";
 import { postBlock } from "../actions/postBlock";
+import { clearBlock } from "../actions/clearBlock";
 
 class BlockForm extends Component {
 
@@ -22,7 +23,15 @@ class BlockForm extends Component {
 
     handleSubmit = (event) => {
         event.preventDefault();
-        this.props.postBlock(this.state, this.props.block)
+        this.props.postBlock(this.state, this.props.block);
+        this.props.clearBlock(this.props.block);
+        this.setState({
+            blockName: '',
+            difficulty: '',
+            username: '',
+            email: ''
+        });
+        console.log('submitted')
     };
     render() {
         return (
@@ -30,17 +39,17 @@ class BlockForm extends Component {
                 <form onSubmit={(event) => this.handleSubmit(event)} className="block-form">
                     <h2>Block Details</h2>
                     Block name:<br/>
-                    <input onChange={(event) => this.handleChange(event)} type="text" name="blockName" /><br/>
-                    <input onChange={(event) => this.handleChange(event)} type="radio" name="difficulty" value="Beginner" />
+                    <input onChange={(event) => this.handleChange(event)} type="text" name="blockName" value={this.state.blockName} /><br/>
+                    <input onChange={(event) => this.handleChange(event)} type="radio" checked={this.state.difficulty === 'Beginner'} name="difficulty" value="Beginner" />
                         Beginner<br/>
-                    <input onChange={(event) => this.handleChange(event)} type="radio" name="difficulty" value="Intermediate" />
+                    <input onChange={(event) => this.handleChange(event)} type="radio" checked={this.state.difficulty === 'Intermediate'} name="difficulty" value="Intermediate" />
                         Intermediate<br/>
-                    <input onChange={(event) => this.handleChange(event)} type="radio" name="difficulty" value="Advanced" />
+                    <input onChange={(event) => this.handleChange(event)} type="radio" checked={this.state.difficulty === 'Advanced'} name="difficulty" value="Advanced" />
                         Advanced<br/><br/>
                     Username:<br/>
-                    <input onChange={(event) => this.handleChange(event)} type="text" name="username" /><br/>
+                    <input onChange={(event) => this.handleChange(event)} type="text" name="username" value={this.state.username}/><br/>
                     Email:<br/>
-                    <input onChange={(event) => this.handleChange(event)} type="email" name="email" /><br/><br/>
+                    <input onChange={(event) => this.handleChange(event)} type="email" name="email" value={this.state.email} /><br/><br/>
                     <input id='create' type="submit" value="Create Block" />
                 </form>
             </div>
@@ -51,4 +60,4 @@ const mapStateToProps = (state) => {
     return { block: state.blockTemplate.blockTemplate }
 };
 
-export default connect(mapStateToProps,{postBlock})(BlockForm);
+export default connect(mapStateToProps,{postBlock, clearBlock})(BlockForm);
