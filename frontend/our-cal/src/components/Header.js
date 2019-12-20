0fcfe8c5f6422296730675e2cccde853b562db19
Audrea Cook <audreacwork@gmail.com>
2019-12-20 14:21:23 -0700

    Add link to home in Header

--- frontend/our-cal/src/components/Header.js
+++ frontend/our-cal/src/components/Header.js
@@ -1,8 +1,9 @@
 import React from "react";
+import {Link} from "react-router-dom";
 
 const Header = () => {
     return (
-        <h1>OurCal: Community Graphghan</h1>
+        <h1><Link to={'/'}>OurCal: Community Graphghan</Link></h1>
     );
 };
 
