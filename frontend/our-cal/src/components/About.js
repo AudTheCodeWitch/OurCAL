daa44b1fcda8e1364f6977ae2d62ffcc9cccf43d
Audrea Cook <audreacwork@gmail.com>
2019-12-20 14:16:45 -0700

    Stub About

---
+++ frontend/our-cal/src/components/About.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const About = () => {
+    return (
+        <div className="About">
+            <h2>About OurCal</h2>
+            <p>OurCal was inspired by my passion for crochet. Blah Blah Blah</p>
+        </div>
+    );
+};
+
+export default About;
