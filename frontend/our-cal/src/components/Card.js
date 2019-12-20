96819ace5469b2152131bd803f0799568fb39e2b
Audrea Cook <audreacwork@gmail.com>
2019-12-20 10:22:20 -0700

    Extract DeleteButton into its own component

--- frontend/our-cal/src/components/Card.js
+++ frontend/our-cal/src/components/Card.js
@@ -1,39 +1,33 @@
-import React, {Component} from 'react';
-import { connect } from 'react-redux'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faTimesCircle } from '@fortawesome/free-regular-svg-icons'
+import React, { Component } from "react";
+import { connect } from "react-redux";
+
 import CompleteBlock from "./CompleteBlock";
 import { destroyBlock } from "../actions/destroyBlock";
+import DeleteButton from "./DeleteButton";
 
 class Card extends Component {
+  render() {
+    return (
+      <div
+        className="card"
+        id={this.props.block.id + "-" + this.props.block.attributes.name}
+      >
+        <DeleteButton
+          destroyBlock={this.props.destroyBlock}
+          id={this.props.block.id}
+        />
 
-    handleDeleteClick = (id) => {
-        this.props.destroyBlock(id)
-    };
-
-    render() {
-        return (
-          <div
-            className="card"
-            id={this.props.block.id + "-" + this.props.block.attributes.name}
-          >
-            <button
-              onClick={e => this.handleDeleteClick(this.props.block.id, e)}
-              className="fa-times-circle"
-            >
-              <FontAwesomeIcon icon={faTimesCircle} />
-            </button>
-            <h3>{this.props.block.attributes.name}</h3>
-            <div className="block">
-              <CompleteBlock block={this.props.block.attributes} />
-            </div>
-            <ul>
-              <li>{this.props.block.attributes.difficulty.rating}</li>
-              <li>{this.props.block.attributes.user.username}</li>
-            </ul>
-          </div>
-        );
-    }
+        <h3>{this.props.block.attributes.name}</h3>
+        <div className="block">
+          <CompleteBlock block={this.props.block.attributes} />
+        </div>
+        <ul>
+          <li>{this.props.block.attributes.difficulty.rating}</li>
+          <li>{this.props.block.attributes.user.username}</li>
+        </ul>
+      </div>
+    );
+  }
 }
 
-export default connect(null, {destroyBlock})(Card);
+export default connect(null, { destroyBlock })(Card);
