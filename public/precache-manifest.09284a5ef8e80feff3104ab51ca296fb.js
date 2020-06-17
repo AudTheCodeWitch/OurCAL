f5bf566b8a708f7c0cf502ffc9315681389e53ca
AudTheCodeWitch <audreacwork@gmail.com>
2020-06-17 15:10:49 -0600

    Deploy to Heroku

---
+++ public/precache-manifest.09284a5ef8e80feff3104ab51ca296fb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1b573f8883d35499a21f1ad6fd5d78c4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7f1d2f5cbaa1737ed595",
+    "url": "/static/css/main.85582990.chunk.css"
+  },
+  {
+    "revision": "9ff3d38caca3bdd50f9d",
+    "url": "/static/js/2.0da23ac5.chunk.js"
+  },
+  {
+    "revision": "058c14a89d808fe8a38664b65e903dd7",
+    "url": "/static/js/2.0da23ac5.chunk.js.LICENSE"
+  },
+  {
+    "revision": "7f1d2f5cbaa1737ed595",
+    "url": "/static/js/main.996280fe.chunk.js"
+  },
+  {
+    "revision": "245ed15c4ca9116b7137",
+    "url": "/static/js/runtime-main.6a1a9803.js"
+  },
+  {
+    "revision": "a6b16278bd4a0c69795b23b0ae5d43c5",
+    "url": "/static/media/CodeWitchLogo.a6b16278.png"
+  }
+]);
