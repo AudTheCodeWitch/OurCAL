e4c8a2cb4505a7b90f132fc5a468d0df5fc80663
Audrea Cook <audreacwork@gmail.com>
2020-06-18 15:03:45 -0600

    Update about page

--- client/src/components/About.js
+++ client/src/components/About.js
@@ -12,27 +12,24 @@ const About = () => {
         can then view one another's blocks and implement their favorites in a
         one-of-a-kind blanket!
       </p>
-      <h2>Future Features</h2>
+      <h2>Coming Soon...</h2>
       <p>
-        Before we launch, there are a few more odds and ends to tie up:
+        We hope to add a few new features in our next release:
         <ul>
-          <li>Add user sessions</li>
+          <li>Add user accounts/sessions</li>
           <li>Add the ability to edit one's submitted patterns</li>
           <li>Add the ability to view patterns by designer</li>
-        </ul>
-        After launch, we'll add some additional features:
-        <ul>
-          <li>View other's patterns in your color scheme</li>
+          <li>View other patterns in your color scheme</li>
           <li>Arrange selected blocks on a blanket template</li>
           <li>Generate written instructions for patterns</li>
           <li>Add a print-friendly view of patterns with instructions</li>
-          <li>
-            Have an idea?{" "}
-            <a href={"https://github.com/AudTheCodeWitch/OurCAL/issues/new"}>
-              Submit a feature request!
-            </a>
-          </li>
         </ul>
+      </p>
+      <p>
+        Have an idea?{" "}
+        <a href={"https://github.com/AudTheCodeWitch/OurCAL/issues/new"}>
+          Submit a feature request!
+        </a>
       </p>
     </div>
   );
